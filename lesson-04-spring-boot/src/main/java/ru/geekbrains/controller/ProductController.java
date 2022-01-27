9d466d25dd515726436cd22be988cae254f5b765
Zagryadskiy Dmitriy <zagryadskiy.dima@gmail.com>
2022-01-27 22:27:00 +0500

    Zagryadskiy_lesson_8_done

--- lesson-04-spring-boot/src/main/java/ru/geekbrains/controller/ProductController.java
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -3,75 +3,79 @@ package ru.geekbrains.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.geekbrains.persist.Product;
-import ru.geekbrains.persist.ProductRepository;
-import ru.geekbrains.persist.ProductSpecification;
+import ru.geekbrains.persist.CategoryRepository;
+import ru.geekbrains.service.ProductService;
+import ru.geekbrains.service.dto.ProductDto;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
 
-    private final ProductRepository productRepository;
-    public static final Logger logger = LoggerFactory.getLogger(Product.class);
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    private final ProductService productService;
+
+    private final CategoryRepository categoryRepository;
 
     @Autowired
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService,
+                             CategoryRepository categoryRepository) {
+        this.productService = productService;
+        this.categoryRepository = categoryRepository;
     }
 
     @GetMapping
     public String listPage(Model model,
                            @RequestParam("nameFilter") Optional<String> nameFilter,
-                            @RequestParam("priceFilter") Optional<String> priceFilter){
+                           @RequestParam("page") Optional<Integer> page,
+                           @RequestParam("size") Optional<Integer> size,
+                           @RequestParam("sort") Optional<String> sort) {
         logger.info("Product filter with name pattern {}", nameFilter.orElse(null));
-        logger.info("Product filter with price pattern {}", priceFilter.orElse(null));
 
-        //TODO
-        Specification<Product> spec = Specification.where(null);
-        if (nameFilter.isPresent() && !nameFilter.get().isBlank()) {
-            spec.and(ProductSpecification.nameLike(nameFilter.get()));
-        }
-
-
-        model.addAttribute("products", productRepository.findAll(spec));
+        model.addAttribute("products", productService.findAll(
+                nameFilter,
+                page.orElse(1) - 1,
+                size.orElse(2),
+                sort.orElse("id")
+        ));
         return "product";
     }
 
     @GetMapping("/{id}")
     public String edit(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("product", productRepository.findById(id)
+        model.addAttribute("product", productService.findById(id)
                 .orElseThrow(() -> new NotFoundException("Product not found")));
+        model.addAttribute("categories", categoryRepository.findAll());
         return "product_form";
     }
 
     @GetMapping("/new")
     public String create(Model model) {
-    model.addAttribute("product", new Product());
+        model.addAttribute("product", new ProductDto());
+        model.addAttribute("categories", categoryRepository.findAll());
         return "product_form";
     }
 
     @PostMapping
-    public String save(@Valid Product product, BindingResult result) {
+    public String save(@Valid ProductDto product, BindingResult result) {
         if (result.hasErrors()) {
             return "product_form";
         }
-            productRepository.save(product);
+        productService.save(product);
         return "redirect:/product";
     }
 
-    @GetMapping("/delete{id}")
-    public String delete (@PathVariable("id") Long id) {
-        productRepository.delete(productRepository.findById(id).get());
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        productService.deleteById(id);
         return "redirect:/product";
     }
 
