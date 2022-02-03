73f6297c0f8b6f483c0544c4ceaa77978d42c196
Zagryadskiy Dmitriy <zagryadskiy.dima@gmail.com>
2022-02-03 22:13:27 +0500

    lesson_10_base

---
+++ frontend/src/app/model/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  constructor(public id:number | null,
+              public name:string,
+              public description:string,
+              public categoryId:number,
+              public categoryName:string,
+              public price:number) {
+  }
+}
